720980b483f56889a9dd885af4f0765ab72446b9
seungyeon <bka0628@naver.com>
2023-02-27 10:24:54 +0900

    Update hyperlinks using DOM Query

--- 08-javascript/app.js
+++ 08-javascript/app.js
@@ -1,3 +1,9 @@
 console.dir(document);
 
 document.body.children[1].children[0].href = "https://naver.com";
+
+let anchorElent = document.getElementById("external-link");
+anchorElent.href = "https://google.com";
+
+anchorElent = document.querySelector('a');
+anchorElent.href = "https://youtube.com/";
