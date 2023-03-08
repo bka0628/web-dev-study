5198ada5a99fd50c0f9e36d20df4f884cb8c6163
seungyeon <bka0628@naver.com>
2023-03-08 12:56:54 +0900

    Add an example of creating a local server using express.js

--- 100일 코딩/10-servers/express-server/app.js
+++ 100일 코딩/10-servers/express-server/app.js
@@ -1,25 +1,18 @@
 // Express 패키지를 사용한 서버
 
-// http 모듈 요청
-const http = require("http");
+// express 모듈 요청
+const express = require("express");
 
-// 요청을 처리하는 함수
-function handleRequest(request, response) {
-  // request : 요청, response : 응답
+// 객체 생성
+const app = express();
 
-  if (request.url === "/currenttime") {
-    // localhost:3000/currenttime
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1>");
-  } else if (request.url == "/") {
-    // localhost:3000
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  }
-}
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+}); // localhost:3000/currenttime
 
-// 서버 객체 생성, 첫번쩨 매개변수 : 요청을 처리하는 함수 전달
-const server = http.createServer(handleRequest);
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+}); // localhost:3000
 
-// 서버가 3000번 포트에서 수신 대기
-server.listen(3000);
+// 3000 포트에서 수신 대기
+app.listen(3000);
