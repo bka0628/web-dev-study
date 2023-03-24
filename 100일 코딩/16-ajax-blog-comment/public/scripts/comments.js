08e0e547e32462fba01e8924cbf8cec669440526
seungyeon <bka0628@naver.com>
2023-03-24 17:01:46 +0900

    Update blog comments based on AJAX response

--- 100일 코딩/16-ajax-blog-comment/public/scripts/comments.js
+++ 100일 코딩/16-ajax-blog-comment/public/scripts/comments.js
@@ -1,10 +1,31 @@
-const loadCommentBtnElement = document.getElementById("load-comments-btn");
+const loadCommentsBtnElement = document.getElementById("load-comments-btn");
+const commentsSectionElement = document.getElementById("comments");
 
-async function fetchCommentsForPost() {
-  const postId = loadCommentBtnElement.dataset.postid;
-  const response = await fetch(`/posts/${postId}/comments`);
-  const responseData = await response.json();
-  console.log(responseData)
+function createCommentsList(comments) {
+  const commentListElement = document.createElement("ol");
+
+  for (const comment of comments) {
+    const commentElement = document.createElement("li");
+    commentElement.innerHTML = `
+    <article class="comment-item">
+      <h2>${comment.title}</h2>
+      <p>${comment.text}</p>
+    </article>
+    `;
+    commentListElement.appendChild(commentElement);
+  }
+
+  return commentListElement;
 }
 
-loadCommentBtnElement.addEventListener("click", fetchCommentsForPost);
+async function fetchCommentsForPost() {
+  const postId = loadCommentsBtnElement.dataset.postid;
+  const response = await fetch(`/posts/${postId}/comments`);
+  const responseData = await response.json();
+
+  const commentsListElement = createCommentsList(responseData);
+  commentsSectionElement.innerHTML = "";
+  commentsSectionElement.appendChild(commentsListElement);
+}
+
+loadCommentsBtnElement.addEventListener("click", fetchCommentsForPost);
