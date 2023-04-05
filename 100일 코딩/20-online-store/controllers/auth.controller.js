3fcb383d4b4672e1da1e3a705ff803bb9bd0e50e
seungyeon <bka0628@naver.com>
2023-04-05 10:03:04 +0900

    Add mongodb package installation and configuration for database connection

--- 100일 코딩/20-online-store/controllers/auth.controller.js
+++ 100일 코딩/20-online-store/controllers/auth.controller.js
@@ -2,11 +2,21 @@ function getSignup(req, res) {
   res.render('customer/auth/signup');
 }
 
+function signup(req, res) {
+  //...
+}
+
 function getlogin(req, res) {
   res.render('customer/auth/login');
 }
 
+function login(req, res) {
+  //...
+}
+
 module.exports = {
   getSignup: getSignup,
+  signup: signup,
   getlogin: getlogin,
+  login: login,
 };
--- 100일 코딩/20-online-store/routes/auth.routes.js
+++ 100일 코딩/20-online-store/routes/auth.routes.js
@@ -6,6 +6,10 @@ const router = express.Router();
 
 router.get('/signup', authController.getSignup);
 
+router.post('/signup', authController.signup);
+
 router.get('/login', authController.getlogin);
+
+router.post('/login', authController.login);
 
 module.exports = router;
