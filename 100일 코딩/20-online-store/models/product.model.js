d22536bc6d2baa84ae8fdc68b81b96bd5229bb61
seungyeon <bka0628@naver.com>
2023-04-14 12:56:15 +0900

    Implement product update functionality for admins

--- 100일 코딩/20-online-store/models/product.model.js
+++ 100일 코딩/20-online-store/models/product.model.js
@@ -8,8 +8,7 @@ class Product {
     this.price = +productData.price;
     this.description = productData.description;
     this.image = productData.image;
-    this.imagePath = `/product-data/images${productData.image}`;
-    this.imageUrl = `/products/assets/images/${productData.image}`;
+    this.updateImageData();
     if (productData._id) {
       this.id = productData._id.toString();
     }
@@ -47,6 +46,11 @@ class Product {
     });
   }
 
+  updateImageData() {
+    this.imagePath = `/product-data/images${this.image}`;
+    this.imageUrl = `/products/assets/images/${this.image}`;
+  }
+
   async save() {
     const productData = {
       title: this.title,
@@ -56,7 +60,24 @@ class Product {
       image: this.image,
     };
 
-    await db.getDb().collection('products').insertOne(productData);
+    if (this.id) {
+      const productId = new mongodb.ObjectId(this.id);
+
+      if (!this.image) {
+        delete productData.image;
+      }
+
+      db.getDb()
+        .collection('products')
+        .updateOne({ _id: productId }, { $set: productData });
+    } else {
+      await db.getDb().collection('products').insertOne(productData);
+    }
+  }
+
+  async replaceImage(newImage) {
+    this.image = newImage;
+    this.updateImageData();
   }
 }
 
