76d443a86be4c225a1a136370dbc58d84d2cd00f
seungyeon <bka0628@naver.com>
2023-04-16 17:19:37 +0900

    Add Product Details Page

--- 100일 코딩/20-online-store/routes/products.routes.js
+++ 100일 코딩/20-online-store/routes/products.routes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.get('/products', productsController.getAllProduct);
 
+router.get('/products/:id', productsController.getProductDetails);
+
 module.exports = router;
