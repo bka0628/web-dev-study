513e0ca0b821f44a623792aa69fb091c2800c3a3
seungyeon <bka0628@naver.com>
2023-04-17 10:39:47 +0900

    add cart logic and new features

--- 100일 코딩/20-online-store/controllers/cart.controller.js
+++ 100일 코딩/20-online-store/controllers/cart.controller.js
@@ -1,7 +1,26 @@
-function addCartItem(req, res, next) {
-    res.locals.cart.addItem();
+const Product = require('../models/product.model');
+const product = require('../models/product.model');
+
+async function addCartItem(req, res, next) {
+  let product;
+  try {
+    product = await Product.findById(req.body.productId);
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  const cart = res.locals.cart;
+
+  cart.addItem(product);
+  req.session.cart = cart;
+
+  res.status(201).json({
+    message: 'Cart updated!',
+    newTotalItems: cart.totalQuantity
+  })
 }
 
 module.exports = {
-    addCartItem: addCartItem
-}
+  addCartItem: addCartItem,
+};
