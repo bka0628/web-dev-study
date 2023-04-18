dedfe58a7c7982f4791fe29d28bb31e710f0c670
seungyeon <bka0628@naver.com>
2023-04-19 08:21:20 +0900

    Update DOM after cart item update via AJAX request

--- 100일 코딩/20-online-store/public/scripts/cart-item-management.js
+++ 100일 코딩/20-online-store/public/scripts/cart-item-management.js
@@ -37,6 +37,20 @@ async function updateCartItem(event) {
   }
 
   const responseData = await response.json();
+
+  if (responseData.updatedCartData.updatedItemPrice === 0) {
+    form.parentElement.parentElement.remove();
+  } else {
+    const cartItemToalPriceElement =
+      form.parentElement.querySelector('.cart-item-price');
+    cartItemToalPriceElement.textContent =
+      responseData.updatedCartData.updatedItemPrice.toFixed(2);
+  }
+
+  cartTotalPriceElement.textContent =
+    responseData.updatedCartData.newTotalPrice.toFixed(2);
+
+  cartBadge.textContent = responseData.updatedCartData.newTotalQuantity;
 }
 
 for (const formElement of cartItemUpdateFormElements) {
