664123649acf607146df4837b0ae8b9b942c701d
seungyeon <bka0628@naver.com>
2023-04-20 13:39:31 +0900

    Fix bugs and fix status codes

--- 100일 코딩/20-online-store/public/scripts/cart-management.js
+++ 100일 코딩/20-online-store/public/scripts/cart-management.js
@@ -1,5 +1,7 @@
-const addToCartButtonElement = document.querySelector('#product-details button');
-const cartBadgeElement = document.querySelector('.nav-items .badge');
+const addToCartButtonElement = document.querySelector(
+  '#product-details button'
+);
+const cartBadgeElements = document.querySelectorAll('.nav-items .badge');
 
 async function addToCart() {
   const productId = addToCartButtonElement.dataset.productid;
@@ -12,11 +14,11 @@ async function addToCart() {
       method: 'POST',
       body: JSON.stringify({
         productId: productId,
-        _csrf: csrfToken
+        _csrf: csrfToken,
       }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+      },
     });
   } catch (error) {
     alert('Sonething went wrong!');
@@ -31,8 +33,10 @@ async function addToCart() {
   const responseData = await response.json();
 
   const newTotalQuantity = responseData.newTotalItems;
-  
-  cartBadgeElement.textContent = newTotalQuantity;
+
+  for (const cartBadgeElement of cartBadgeElements) {
+    cartBadgeElement.textContent = newTotalQuantity;
+  }
 }
 
 addToCartButtonElement.addEventListener('click', addToCart);
